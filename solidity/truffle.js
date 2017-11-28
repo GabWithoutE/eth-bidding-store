651706c02dfb4db6be086c4a75c1130a7cfc0eeb
Adam Tache <at201@duke.edu>
2017-11-27 22:08:58 -0500

    Truffle config file for local development and some cleanup

---
+++ solidity/truffle.js
@@ -0,0 +1,15 @@
+module.exports = {
+   networks: {
+       development: {
+       host: "localhost",
+       port: 8545,
+       network_id: "*",
+       gas: 4712388
+   },
+	rpc: {
+		host: "localhost",
+		gas: 4712388,
+		port: 8545
+	}
+ }
+};
